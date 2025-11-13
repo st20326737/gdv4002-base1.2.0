cdc6e4f4c8b51b33069a8dacf5950b2848217fce
st20326737 <st20326737@outlook.cardiffmet.ac.uk>
2025-11-13 15:57:19 +0000

    Added astrods base

---
+++ gdv4002-base1/gdv4002-base1/AstrodsBase.cpp
@@ -0,0 +1,70 @@
+#include "AstrodsBase.h"
+
+AstrodsBase::AstrodsBase() : GameObject2D()
+{
+	damage = 0;
+	health = 0;
+	mass = 0.0f;
+	acceleration = 0.0f;
+	isDead = true;
+}
+
+AstrodsBase::AstrodsBase(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, int initDamage, int initHealth, float initMass, float initAcceleration, bool initIsDead) : GameObject2D(initPosition, initOrientation, initSize, initTextureID)
+{
+
+	setDamage(initDamage);
+	setHealth(initHealth);
+	setMass(initMass);
+	setAcceleration(initAcceleration);
+	setIsDead(initIsDead);
+
+}
+
+AstrodsBase::~AstrodsBase()
+{
+	// Destructor logic if needed
+}
+
+// Getter methods
+int AstrodsBase::getDamage() const
+{
+	return this->damage;
+}
+int AstrodsBase::getHealth() const
+{
+	return this->health;
+}
+float AstrodsBase::getMass() const
+{
+	return this->mass;
+}
+float AstrodsBase::getAcceleration() const
+{
+	return this->acceleration;
+}
+bool AstrodsBase::getIsDead() const
+{
+	return this->isDead;
+}
+
+// Setter methods
+void AstrodsBase::setDamage(int initDamage)
+{
+	this->damage = initDamage;
+}
+void AstrodsBase::setHealth(int initHealth)
+{
+	this->health = initHealth;
+}
+void AstrodsBase::setMass(float initMass)
+{
+	this->mass = initMass;
+}
+void AstrodsBase::setAcceleration(float initAcceleration)
+{
+	this->acceleration = initAcceleration;
+}
+void AstrodsBase::setIsDead(bool initIsDead)
+{
+	this->isDead = initIsDead;
+}
---
+++ gdv4002-base1/gdv4002-base1/AstrodsBase.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Engine.h"
+#include "GameObject2D.h"
+
+
+class AstrodsBase : public GameObject2D
+{
+private:
+	int damage;
+	int health;
+	float mass;
+	float acceleration;
+	bool isDead;
+
+public:
+	AstrodsBase();
+	AstrodsBase(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, int initDamage, int initHealth, float initMass, float initAcceleration, bool initIsDead);
+	~AstrodsBase();
+
+	// Getter methods
+	int getDamage() const;
+	int getHealth() const;
+	float getMass() const;
+	float getAcceleration() const;
+	bool getIsDead() const;
+
+	// Setter methods
+	void setDamage(int initDamage);
+	void setHealth(int initHealth);
+	void setMass(float initMass);
+	void setAcceleration(float initAcceleration);
+	void setIsDead(bool initAlive);
+
+	};
+};
+
